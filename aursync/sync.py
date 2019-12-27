d12d076c2f5f5765bf9e4a439cfb17254680cdca
Zenith00 <zenith00dev@gmail.com>
2019-12-26 23:03:16 -0800

    remove circular import

--- aursync/sync.py
+++ aursync/sync.py
@@ -13,7 +13,6 @@ import jsonpickle  # type:ignore
 
 import aursync.flattener as flattener
 import aursync.mpmc as mpmc
-from aursync.config_proxy import _ConfigProxy
 
 # from typing import Iterator, _T_co, _KT, _VT_co, _VT
 # from typing import Iterator, _T_co, _KT, _VT_co, _VT
@@ -28,6 +27,50 @@ _FLAG_FIRST = object()
 
 # noinspection PyPep8Naming
 
+class _ConfigProxy:
+
+    def __init__(self, sync: Sync, key_root):
+        self.key_root: str = key_root
+        self.hm_key = ""
+        self.sync = sync
+
+    def _verify_redis(self) -> None:
+        if not self.sync.ready:
+            raise RuntimeError("Sync redis not ready!")
+        if not self.sync.redis:
+            raise RuntimeError("<???>: Sync is ready but redis is None")
+        if self.sync.redis.closed:
+            raise RuntimeError("Sync redis is closed!")
+
+    def __setitem__(self, k: flattener.FlatKey, v: str) -> None:
+        self._verify_redis()
+        self.compose_key(k)
+
+        assert self.sync.redis is not None  # dummy for Mypy
+        self.sync.redis.hmset(self.key_root, self.hm_key, v)
+
+    def compose_key(self, k) -> None:
+        sep: flattener.FlatContainerType
+        sep = list if isinstance(k, int) else dict
+        self.hm_key = flattener.compose_keys(self.hm_key, k, sep)
+
+    def __delitem__(self, k: flattener.FlatKey) -> None:
+        self._verify_redis()
+        self.compose_key(k)
+
+        assert self.sync.redis is not None  # dummy for Mypy
+        self.sync.redis.hdel(self.key_root, self.hm_key)
+
+    def __getitem__(self, k: flattener.FlatKey) -> _ConfigProxy:
+        self._verify_redis()
+        self.compose_key(k)
+
+        assert self.sync.redis is not None  # dummy for  Mypy
+        return self
+
+    def __str__(self) -> str:
+        assert self.sync.redis is not None  # dummy for  Mypy
+        return self.sync.redis.hmget(self.key_root, self.hm_key)
 
 def _listify_arg(listy: ty.Optional[ty.Union[str, ty.Iterable[ty.Any]]]
                  ) -> ty.Iterable[ty.Any]:
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aursync",
-    version="0.0.2",
+    version="0.0.4",
     author="Zenith00",
     author_email="Zenith00dev@gmail.com",
     description="Synchronization library for Aurora",
